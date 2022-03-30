738851e9ee487b99420ed363ef058f7bcaf76549
ldsodi <ldsodi@wustl.edu>
2022-03-29 19:52:42 -0500

    moved functions to game.class

--- guessing_game.java
+++ guessing_game.java
@@ -1,60 +1,31 @@
+import java.util.Scanner;
+//issue with importing Game.class for some reason
 
-class guessing_game {
+public class guessing_game {
     public static void main(String[] args) {
         int guess = 0;
         int lastGuess = 0;
-        int answer = generateAnswer();
+        boolean play = true;
+        
+        Game myGame = new Game();
 
-        String hint = generateHint(answer);
+        String hint = myGame.generateHint();
         System.out.println(hint);
 
-        if(checkGuess(guess, answer)){
-            
-        }
-        else {
-            checkDistance(guess, lastGuess, answer);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Make your guess: ");
+        lastGuess = guess;
+        guess = scan.nextInt();
+
+        while(play){
+            if(myGame.checkGuess(guess)){
+                play = false;
+            }
+            else if(myGame.turn > 1) {
+                System.out.println(myGame.checkDistance(guess, lastGuess));
+            }
         }
 
-
-    }
-
-    static int generateAnswer() {
-        return (int)Math.random()*25+1;
-    }
-
-    static boolean checkGuess(int guess, int answer) {
-        if (guess == answer){
-            System.out.println("Congratulations! That's correct!");
-            return true;
-        }
-        System.out.println("Wrong!");
-        return false;
-    }
-
-    static void checkDistance(int guess, int lastGuess, int answer) {
-        if (Math.abs(guess - answer) < Math.abs(lastGuess - answer)) {
-            System.out.println("Warmer");
-        } else if (Math.abs(guess - answer) > Math.abs(lastGuess - answer)) {
-            System.out.println("Colder");
-        } else {
-            System.out.println("You guessed the same thing twice!");
-        }
-    }
-
-    static String generateHint(int answer) {
-        int way = (int)Math.random()*2;
-        String hint = "";
-        if(way == 1){
-            int num1 = (int)Math.random()*answer + 1;
-            int num2 = answer - num1;
-            hint = "Hint: " + num1 + " + " + num2;
-        }
-        else{
-            int num1 = (int)Math.random()*100 + answer;
-            int num2 = num1 - answer;
-            hint = "Hint: " + num1 + " - " + num2;
-        }
-        return hint;
     }
 }
 
